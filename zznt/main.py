033b5bb42a1a9d8216900285518ec52e9f5a52d8
sunao <sunao_0626@hotmail.com>
2018-04-12 02:20:56 +0800

    enable google

--- zznt/main.py
+++ zznt/main.py
@@ -14,6 +14,8 @@ if not os.path.exists("./files/"):
 GLOBAL_USER_QUEUE = []
 GLOBAL_MESSAGE_QUEUE = {}
 
+print(search_google("腾讯", lambda x: 0))
+exit()
 
 def print_help(config):
     response = "参考: [at瓦力]"
