00552f37160ac3e23293023fd87f792227e41d28
sunao <sunao_0626@hotmail.com>
2018-04-12 02:28:21 +0800

    enable google

--- zznt/utils/html_to_image.py
+++ zznt/utils/html_to_image.py
@@ -4,9 +4,15 @@ import time
 
 
 def download_web_to_image(url):
-    imgkit.from_url(url, "./search-output.jpg", options={"width": 800, "quality": 50})
-    exit(10)
+    try:
+        imgkit.from_url(url, "./search-output.jpg", options={"width": 800, "quality": 50})
+        return 1
+    except:
+        return 0
 
 
 if __name__ == '__main__':
-    download_web_to_image(os.sys.argv[1])
+    if 1 == download_web_to_image(os.sys.argv[1]):
+        exit(10)
+    else:
+        exit(0)
